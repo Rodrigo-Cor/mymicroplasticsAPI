2ade70188ccaf3c53ddb525a6f49e4f21ad72487
Rodrigo Corona <rorrecorona78@gmail.com>
2023-04-14 14:51:55 -0600

    PruebaImport

--- ConexionAPI/index.js
+++ ConexionAPI/index.js
@@ -1,13 +1,15 @@
+import {parseStringPromise} from 'xml2js'
+import {get} from 'axios'
+
 module.exports = async function (context, req) {
     // Definir la URL base de la API de PubMed
     const baseUrl = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
 
     // Definir los parámetros de búsqueda
     const searchTerm = "color psychology"
-    const apiKey = "Holaa"
+    const apiKey = process.env.API_KEY
     const searchUrl = `${baseUrl}esearch.fcgi?db=pubmed&api_key=${apiKey}&term=${searchTerm}&free_full_text=yes`
     context.log('JavaScript HTTP trigger function processed a request.');
-
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage =  "Hello, " + name + ". This HTTP triggered function executed successfully."
     + "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response." + searchUrl;
