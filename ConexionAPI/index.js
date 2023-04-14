ae5f265d9b9d264d1407978197ccb854bd5efa89
Rodrigo Corona <rorrecorona78@gmail.com>
2023-04-14 00:28:58 -0600

    V2

--- ConexionAPI/index.js
+++ ConexionAPI/index.js
@@ -14,7 +14,7 @@ module.exports = async function (context, req) {
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response." + searchUrl;
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
