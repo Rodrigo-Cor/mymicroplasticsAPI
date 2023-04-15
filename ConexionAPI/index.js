d9f1aa3bdd582900060f2138b904331ed4494adf
Rodrigo Corona <rorrecorona78@gmail.com>
2023-04-15 00:04:12 -0600

    ConexionAcabada

--- ConexionAPI/index.js
+++ ConexionAPI/index.js
@@ -1,14 +1,17 @@
 const { parseStringPromise } = require("xml2js");
-const axios = require('axios');
-const apiKey = process.env.KEY;
+const axios = require("axios");
+//const apiKey = process.env.KEY;
 
 module.exports = async function (context, req) {
     // Definir la URL base de la API de PubMed
     const baseUrl = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/";
 
+    //Solo para debugger
+    const apiKey = "445f5e20db09bea235c4eb9b04c2e43d6d09";
+
     // Definir los parámetros de búsqueda
     const searchTerm = "psychology";
-    
+
     const searchUrl = `${baseUrl}esearch.fcgi?db=pubmed&api_key=${apiKey}&term=${searchTerm}&free_full_text=yes`;
     context.log('JavaScript HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
@@ -19,15 +22,23 @@ module.exports = async function (context, req) {
     try {
         const response = await axios.get(searchUrl);
         const json = await parseStringPromise((response.data), { mergeAttrs: true });
+        const ids = json["eSearchResult"]["IdList"][0]["Id"];
+        let num = Math.floor(Math.random()*(ids.length));
+        let articles = [];
+        for (let j = 0; j < 3; j++) {
+            articles.push(ids[num]);
+            num++;
+            if (num >= ids.length)
+                num = 0;    
+        }
         context.res = {
             // status: 200, /* Defaults to 200 */
-            status:200,
-            body: json
+            body: articles
         };
     } catch (error) {
         context.res = {
             status: 400,
-            body: error
+            body: "Error en la conexión al API"
         }
     }
 };
