77b97ff6e789ae9a0bcc8d247a39a721ec4d30fb
Rodrigo Corona <rorrecorona78@gmail.com>
2023-04-14 19:07:34 -0600

    Repeticion

--- ConexionAPI/index.js
+++ ConexionAPI/index.js
@@ -1,52 +1,21 @@
-//const { get } = require("axios");
-//const { parseStringPromise } = require("xml2js");
+const { parseStringPromise } = require("xml2js");
+const axios = require('axios');
+const apiKey = process.env.KEY;
 
-//const apiKey = process.env.KEY;
-
-export default async function (context, req) {
-    context.log('Node.js HTTP trigger function processed a request. RequestUri=');
-
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    /*
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = async function (context, req) {
     // Definir la URL base de la API de PubMed
     const baseUrl = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
 
     // Definir los parámetros de búsqueda
     const searchTerm = "color psychology"
-    const searchUrl = `${baseUrl}esearch.fcgi?db=pubmed&api_key=${apiKey}&term=${searchTerm}&free_full_text=yes`
     
+    const searchUrl = `${baseUrl}esearch.fcgi?db=pubmed&api_key=${apiKey}&term=${searchTerm}&free_full_text=yes`
+    context.log('JavaScript HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
-
+    const responseMessage =  "Hello, " + name + ". This HTTP triggered function executed successfully."
+    + "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response." + searchUrl;
     context.res = {
-        body: searchUrl
-    };
-    //const response = await axios.get();
-    */
-    /*
-    axios.get(searchUrl)
-        .then((response) => {
-            const responseMessage = response
-            context.res = {
-                body: responseMessage
-            };
-        })
-        .catch((error) => {
-            context.res = {
-                status: 400,
-                body: error
-            };
-        });
-    */
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    }; 
 };
