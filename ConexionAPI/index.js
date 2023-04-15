b0347e25dcbc7e6f57f3d28d71c9982a0e4ee631
Rodrigo Corona <rorrecorona78@gmail.com>
2023-04-14 18:38:25 -0600

    Prueba2

--- ConexionAPI/index.js
+++ ConexionAPI/index.js
@@ -3,18 +3,18 @@ import axios from 'axios';
 const apiKey = process.env.KEY;
 
 export default async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
     // Definir la URL base de la API de PubMed
     const baseUrl = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
 
     // Definir los parámetros de búsqueda
     const searchTerm = "color psychology"
     const searchUrl = `${baseUrl}esearch.fcgi?db=pubmed&api_key=${apiKey}&term=${searchTerm}&free_full_text=yes`
-    //context.log('JavaScript HTTP trigger function processed a request.');
     //const name = (req.query.name || (req.body && req.body.name));
     context.res = {
         body: searchUrl
     };
-    
+    const response = await axios.get();
     /*
     axios.get(searchUrl)
         .then((response) => {
