1da0d9e86e9e9c2994434bef65d3fc2300941d1b
Rodrigo Corona <rorrecorona78@gmail.com>
2023-04-16 20:26:28 -0600

    NuevaFunctionID

--- GetIDArticles/index.js
+++ GetIDArticles/index.js
@@ -1,4 +1,4 @@
-const send = require("../globalFunctions");
+const { send, get3Element } = require("../globalFunctions");
 const { parseStringPromise } = require("xml2js");
 const axios = require("axios");
 const apiKey = process.env.KEY;
@@ -9,36 +9,46 @@ module.exports = async function GetIDArticles(context, req) {
 
     //Solo para debugger
     //const apiKey = "445f5e20db09bea235c4eb9b04c2e43d6d09";
+    //const searchTerm = "Microplastics biodegradation by biofloc-producing bacteria: An inventive biofloc technology approach"
+    //const typeRequest = 1;
+    //const articlesNum = 3;
 
     // Definir los parámetros de búsqueda
-    const searchTerm = "microplastics water";
+    const searchTerm = req.body && req.body.searchTerm
+    const typeRequest = req.body && req.body.typeRequest
+    const articlesNum = req.body && req.body.articlesNum
 
-    const searchUrl = `${baseUrl}esearch.fcgi?db=pubmed&api_key=${apiKey}&term=${searchTerm}&free_full_text=yes`;
+    // Definir la URL de la petición
+    let searchUrl = `${baseUrl}esearch.fcgi?db=pubmed&api_key=${apiKey}&term=${searchTerm}&free_full_text=yes`;
     try {
+        if (typeRequest === 1)
+            searchUrl = `${baseUrl}esearch.fcgi?db=pubmed&api_key=${apiKey}&term=${searchTerm}[Title/Abstract]`;
         const response = await axios.get(searchUrl);
         const json = await parseStringPromise((response.data), { mergeAttrs: true });
         const ids = json["eSearchResult"]["IdList"][0]["Id"];
-        let num = Math.floor(Math.random() * (ids.length));
-        let articles = [];
-        for (let j = 0; j < 3; j++) {
-            articles.push(ids[num]);
-            num++;
-            if (num >= ids.length)
-                num = 0;
+
+        const articles = get3Element(ids, articlesNum)
+        const articlesData = []
+
+        for (id of articles) {
+            const articleUrl = `${baseUrl}efetch.fcgi?db=pubmed&id=${id}&api_key=${apiKey}&retmode=xml`;
+            const responseDataArticles = await axios.get(articleUrl);
+            const jsonDataArticles = await parseStringPromise(responseDataArticles.data, { mergeAttrs: true });
+            articlesData.push({
+                title: jsonDataArticles["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["ArticleTitle"][0],
+                author: jsonDataArticles["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"][0]["Author"].map((author) => author["LastName"][0] + " " + author["Initials"][0]).join(", "),
+                link: `https://pubmed.ncbi.nlm.nih.gov/${id}/`,
+            });
         }
-        return send(200, articles);
-        /*context.res = {
-            // status: 200, /* Defaults to 200 
-            body: articles
-        };
-        */
-    } catch (error) {
-        return send(404, "Error en la obtención de los ID's");
-        /*
         context.res = {
-            status: 400,
+            // status: 200, /* Defaults to 200 
+            body: articlesData
+        };
+    } catch (error) {
+        //return send(404, "Error en la obtención de los ID's");
+        context.res = {
+            status: 404,
             body: "Error en la conexión al API"
         }
-        */
     }
 };
--- globalFunctions.js
+++ globalFunctions.js
@@ -1,6 +1,26 @@
-module.exports = function send(status, body){
+function send(status, body) {
     return {
         status,
         body,
     };
+};
+
+function get3Element(arr, num) {
+    const length = arr.length
+    const newArr = []
+    if (arr.length <= num) {
+        return arr;
+    }
+
+    for (let i = 0; i < num; i++) {
+        const index = Math.floor(Math.random() * length)
+        const element = arr.splice(index, 1)[0];
+        newArr.push(element)
+    }
+    return newArr
+};
+
+module.exports = {
+    send,
+    get3Element
 };
